b6767f1cdf86deadbb956156678dc026b0895258
Gangadhar Marka <markagangadhar7@gmail.com>
2024-11-11 15:25:01 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -2,6 +2,9 @@ class Solution(object):
     def isValid(self, s):
         stack = []
         mapping = {')':'(','{':'}',']':'['}
+        for char in mapping:
+            if char in mapping:
+                
         """
         :type s: str
         :rtype: bool
