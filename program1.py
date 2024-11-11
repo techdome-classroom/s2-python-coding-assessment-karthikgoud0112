075263ba989aa1345c29079fc479209aac82cf91
Gangadhar Marka <markagangadhar7@gmail.com>
2024-11-11 15:32:46 +0530

    Auto commit on save

--- program1.py
+++ program1.py
@@ -1,25 +1,12 @@
-class Solution(object):
+class Solution:
     def isValid(self, s):
         stack = []
-        mapping = {')':'(','{':'}',']':'['}
-        for char in mapping:
+        mapping = {')': '(', '}': '{', ']': '['}
+        for char in s:
             if char in mapping:
                 top_element = stack.pop() if stack else '#'
-                if mapping[char]!= top_element:
+                if mapping[char] != top_element:
                     return False
-                else:
-                    stack.append(char)
-        return not stack            
-
-
-
-
-
-
-
-    
-
-
-
-  
-
+            else:
+                stack.append(char)
+        return not stack
